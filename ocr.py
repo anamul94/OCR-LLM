19ad3eee169bbebb3b5aaeb867e3049b2763f4ad
Anamul Haque <anamul.ice14@gmail.com>
2025-02-24 22:15:07 +0600

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -35,7 +35,7 @@ def input_image_setup(uploaded_file):
         raise FileNotFoundError("No file uploaded")
 ##initialize our streamlit app
 
-st.set_page_config(page_title="Gemini OCR App")
+st.set_page_config(page_title="OCR App")
 
 st.header("Gemini OCR App")
 # input=st.text_input("Input Prompt: ",key="input")
